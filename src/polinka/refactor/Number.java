39d99b0d0237e50655b33eda19ab4e893b564c89
Polycarpik <paladyina@gmail.com>
2015-11-10 21:02:45 +0200

    second stage

---
+++ src/polinka/refactor/Number.java
@@ -0,0 +1,18 @@
+package polinka.refactor;
+
+class Number {
+
+    private String number;
+
+    public Number(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
